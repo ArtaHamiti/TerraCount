9ca7db3ea6946a88aaf59712cfa97037883a4b4b
ArtaHamiti <a-hamiti@hotmail.com>
2024-12-09 11:54:09 +1100

    changed to using Jinja2Templates

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from typing import Union
 from csv import DictReader
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from jinja2 import Environment, FileSystemLoader
 import logging
 
-environment = Environment(loader=FileSystemLoader("templates/"))
+from starlette.templating import Jinja2Templates
+
+
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 # Mount the static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-def read_root():
-    template = environment.get_template("frontpage.html.j2")
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
     with open("Terraforming_score_sheet.csv") as f:
         games = DictReader(f, delimiter=',', quotechar='"')
         users = [game["Name"] for game in games]
-    return HTMLResponse(template.render({"users": users}))
+    return templates.TemplateResponse(request, name="frontpage.html.j2", context={"users": users})
 
-
+"""
 @app.get("/games2")
 def read_root():
     template = environment.get_template("all_games.html.j2")
@@ -49,7 +52,7 @@ def read_root():
 def read_item(game_id: str, q: Union[str, None] = None):
     return {"game_id": game_id, "q": q}
 
-
+"""
 @app.post("/all_games/")
 def read_item(game: str = Form()):
     return game
