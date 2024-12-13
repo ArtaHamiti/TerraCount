b656107828f36f13db9518bd407374e27381f0f6
ArtaHamiti <a-hamiti@hotmail.com>
2024-12-14 10:28:44 +1100

    trying to make a good dynamic form

--- static/testing_new_game.js
+++ static/testing_new_game.js
@@ -1,23 +1,32 @@
 document.getElementById('addButton').addEventListener('click', function() {
-  // Create a new div element to hold the new input field
-  var newFormGroup = document.createElement('div');
-  newFormGroup.className = 'form-group';
 
-  players = document.getElementById('players').value;
-  console.log("playerssss:", players)
+    players = document.getElementById('players').value;
+    console.log("Players:", players)
 
-  for (let i = 0; i <players; i++ ) {
-    // Create a new input element
-    var newField = document.createElement('input');
-    newField.type = 'text';
-    newField.name = 'dynamicField';
+    if (players == 1) {
+        document.write("Great, you went solo!");
+        console.log("if statement DONE");
+    }  else {
+        document.write("Okay, I have registered ", players, " players!");
 
-    console.log("iteration:", i)
-    // Append the new input element to the new div
-    newFormGroup.appendChild(newField);
+        // Create a new div element to hold the new input field
+        var newFormGroup = document.createElement('div');
+        newFormGroup.className = 'form-group';
 
-    // Append the new div to the form
-    document.getElementById('dynamicForm').appendChild(newFormGroup);
-  }
+        for (let i = 0; i <players; i++ ) {
+            console.log("check number 1")
+            // Create a new input element
+            /*
+            var newField = document.createElement('input');
+            newField.type = 'text';
+            newField.name = 'dynamicField';
 
+            console.log("iteration:", i)
+            // Append the new input element to the new div
+            newFormGroup.appendChild(newField);
+
+            // Append the new div to the form
+            document.getElementById('dynamicForm').appendChild(newFormGroup); */
+        }
+    }
 });
